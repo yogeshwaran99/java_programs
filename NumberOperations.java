ff78640d28090c451bba6897ea432263e376e627
yogeshwaran99 <itsyogeshwaran@gmail.com>
2025-08-22 10:33:12 +0530

    Add a program for NumberOperations

---
+++ NumberOperations.java
@@ -0,0 +1,47 @@
+public class NumberOperations {
+  public static void main(String[] args) {
+
+    int n=10;
+
+    // 1. Check if number is positive, negative or zero
+    if(n>0) {
+      System.out.println(n+" is positive");
+    }else if (n<0){
+      System.out.println(n+" is negative");      
+    }else{
+      System.out.println("The number is zero");
+    }
+
+    // 2. Check if number is even or odd
+    if (n%2==0) {
+      System.out.println(n+ " is Even");
+    } else {
+      System.out.println(n+ " is Odd");
+    }
+
+    // 3. Check if number is prime
+    if (isPrime(n)) {
+      System.out.println(n+ " is Prime");
+    } else {
+      System.out.println(n+ " is Not Prime");
+    }
+
+    // 4. Print numbers 10 to 1 without increment operator
+    int i=10;
+    while (n>=1) {
+      System.out.println(n);
+      n=n-1;
+    }
+  }
+
+  //helper method for prime check
+  public static boolean isPrime(int n) {
+    if (n<=1) return false;
+    for (int i=2;i<=n/2;i++) {
+      if (n%i==0) return false;
+    }
+    return true;
+  }   
+
+}
+
